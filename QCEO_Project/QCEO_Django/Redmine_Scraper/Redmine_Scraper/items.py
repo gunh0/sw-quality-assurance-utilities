33423b9e81772c3937c85e25ee6d89ffa6d8219e
devgunho <parkgh3737@naver.com>
2020-04-01 10:35:05 +0900

    Adding... Redmine Scraper : add items.py

--- QCEO_Project/QCEO_Django/Redmine_Scraper/Redmine_Scraper/items.py
+++ QCEO_Project/QCEO_Django/Redmine_Scraper/Redmine_Scraper/items.py
@@ -6,9 +6,20 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-
+from scrapy.item import Item, Field
 
 class RedmineScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+    number = scrapy.Field()
+    project = scrapy.Field()
+    proj_type = scrapy.Field()
+    proj_status = scrapy.Field()
+    priority = scrapy.Field()
+    title = scrapy.Field()
+    author = scrapy.Field()
+    master = scrapy.Field()
+    category = scrapy.Field()
+    target_version = scrapy.Field()
+    change = scrapy.Field()
+    start_time = scrapy.Field()
+    deadline = scrapy.Field()
     pass
