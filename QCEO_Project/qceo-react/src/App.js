4283373313d4505700efa5358c3fbb7e06774e70
devgunho <parkgh3737@naver.com>
2020-04-07 13:48:15 +0900

    Adding Login...

--- QCEO_Project/qceo-react/src/App.js
+++ QCEO_Project/qceo-react/src/App.js
@@ -1,5 +1,16 @@
 import React, {Component} from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+
 import Dashboard from './components/Dashboard'
+import LoginForm from './components/auth/LoginForm';
+import RegisterForm from './components/auth/RegisterForm';
+import PrivateRoute from './common/PrivateRoute';
+
+import { loadUser } from './actions/auth';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import history from './history';
 
 class App extends Component {
     state = {
@@ -14,13 +25,23 @@ class App extends Component {
         } catch (e) {
             console.log(e);
         }
+        store.dispatch(loadUser());
     }
 
     render() {
-        return (<div>
-          <Dashboard/>
-          </div>);
+        return (
+            <Provider store={store}>
+                <Router history={history}>
+                    <Switch>
+                        <PrivateRoute exact="exact" path='/' component={Dashboard}/>
+                        <Route exact="exact" path='/dashboard' component={Dashboard}/>
+                        <Route exact="exact" path='/login' component={LoginForm}/>
+                        <Route exact="exact" path='/register' component={RegisterForm}/>
+                    </Switch>
+                </Router>
+            </Provider>
+        );
     }
-  }
+}
 
-  export default App;
+export default App;
