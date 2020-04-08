e790444e2fe6e8a5a41eeb86b44a09f0017b08a9
devgunho <parkgh3737@naver.com>
2020-04-08 15:27:58 +0900

    Add Sign In Form / Sign up Form

--- QCEO_Project/qceo-react/src/components/auth/LoginForm.js
+++ QCEO_Project/qceo-react/src/components/auth/LoginForm.js
@@ -1,8 +1,53 @@
 import React, {Component} from 'react';
+
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
 import {Link, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {Field, reduxForm} from 'redux-form';
 import {login} from '../../actions/auth';
+
+function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://material-ui.com/">
+          Your Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
 
 class LoginForm extends Component {
     renderField = ({
@@ -44,7 +89,6 @@ class LoginForm extends Component {
             .props
             .login(formValues);
     };
-
     render() {
         if (this.props.isAuthenticated) {
             return <Redirect to='/'/>;
@@ -70,13 +114,56 @@ class LoginForm extends Component {
                         <Field name='non_field_errors' type='hidden' component={this.hiddenField}/>
                         <button className='ui primary button'>Login</button>
                     </form>
-                    <p
-                        style={{
-                            marginTop: '1rem'
-                        }}>
-                        Don't have an account?
-                        <Link to='/register'>Register</Link>
-                    </p>
+                    <Container component="main" maxWidth="xs">
+                        <CssBaseline/>
+                        <div className={useStyles.paper}>
+                            <Typography component="h1" variant="h5">
+                                Sign in
+                            </Typography>
+                            <form className={useStyles.form} noValidate="noValidate">
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required="required"
+                                    fullWidth="fullWidth"
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocus="autoFocus"/>
+                                <TextField
+                                    variant="outlined"
+                                    margin="normal"
+                                    required="required"
+                                    fullWidth="fullWidth"
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"/>
+                                <Button
+                                    type="submit"
+                                    fullWidth="fullWidth"
+                                    variant="contained"
+                                    color="primary"
+                                    className={useStyles.submit}>
+                                    Sign In
+                                </Button>
+                                <Grid container="container">
+                                    <Grid item="item" xs="xs">
+                                    </Grid>
+                                    <Grid item="item">
+                                        <Link to="/register" variant="body2">
+                                            {"Don't have an account? Sign Up"}
+                                        </Link>
+                                    </Grid>
+                                </Grid>
+                            </form>
+                        </div>
+                        <Box mt={8}>
+                            <Copyright/>
+                        </Box>
+                    </Container>
                 </div>
             </div>
         );
