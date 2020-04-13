66f1d512ebee3258af9fbdedd6a31b03e43bc653
devgunho <parkgh3737@naver.com>
2020-04-13 17:26:21 +0900

    commit

--- QCEO_Project/qceo-frontend-react/src/RedmineBase_QualityInspection/StepProgress.js
+++ QCEO_Project/qceo-frontend-react/src/RedmineBase_QualityInspection/StepProgress.js
@@ -1,8 +1,17 @@
 import React from "react";
 
+import { Progress } from 'react-sweet-progress';
+import "react-sweet-progress/lib/style.css";
+
 const StepProgress = () => {
     return (
-       <div>Setp Progress</div>
+        <div>
+            <Progress percent={50} />
+            <Progress percent={25} />
+            <Progress percent={75} />
+            <Progress percent={99} />
+            <Progress percent={100} />
+        </div>
     );
 }
 
