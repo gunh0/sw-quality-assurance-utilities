a2866184bba92e0adaaeb75ed9b896476993e197
devgunho <parkgh3737@naver.com>
2020-05-14 15:51:06 +0900

    modify ftp

--- UnityManager/ftp_test.py
+++ UnityManager/ftp_test.py
@@ -5,14 +5,19 @@ target_server = "192.168.155.183"  # Redhat 7.3
 userid = "test04"
 pwd = "1qa2ws3ed$"
 
+# Anonymous
+print('Anonymous FTP ')
 ftp = FTP(target_server)
 ftp.login()
 ftp.retrlines('LIST')
+print(ftp.pwd())
 ftp.quit()
 
+# user
+print('\nUser FTP ')
 uftp = FTP(target_server)
 uftp.login(userid, pwd)
-uftp.retrlines('LIST')
+print(uftp.pwd())
 uftp.quit()
 
 print('---------------------------FTP TESTING END---------------------------')
